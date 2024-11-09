885773a38e4adead4df90d4c89065f2f54647d53
Eduardo Alves <edu0alves00@gmail.com>
2024-11-09 19:00:30 -0300

    feat: aply styles

--- src/screens/Cest.js
+++ src/screens/Cest.js
@@ -5,8 +5,10 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  View,
 } from "react-native";
 import top from "../../assets/topo.png";
+import logo from "../../assets/logo.png";
 
 // Função utilizada para pegar o tamanho da tela
 const width = Dimensions.get("screen").width;
@@ -17,6 +19,19 @@ const Cest = () => {
       <SafeAreaView>
         <Image source={top} style={styles.top} />
         <Text style={styles.title}>Detalhes da Cesta</Text>
+
+        <View style={styles.cest}>
+          <Text style={styles.name}>Cesta de Verduras</Text>
+          <View style={styles.farm}>
+            <Image style={styles.imageFarm} source={logo} />
+            <Text style={styles.nameFarm}>Jenny Jack Farm</Text>
+          </View>
+          <Text style={styles.description}>
+            Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+            para a sua cozinha.
+          </Text>
+          <Text style={styles.price}>R$40,00</Text>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -36,7 +51,42 @@ const styles = StyleSheet.create({
     lineHeight: 26,
     color: "white",
     fontWeight: "bold",
-    padding: 16
+    padding: 16,
+  },
+  cest: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  name: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold",
+  },
+  farm: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imageFarm: {
+    width: 32,
+    height: 32,
+  },
+  nameFarm: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+  },
+  description: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  price: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
   },
 });
 
